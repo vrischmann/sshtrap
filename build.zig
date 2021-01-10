80fcfe8d016e0f27759cd63ba1ab504bb115c085
Vincent Rischmann <vincent@rischmann.fr>
2021-01-10 17:27:02 +0100

    update build.zig

--- build.zig
+++ build.zig
@@ -1,27 +1,38 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+pub fn build(b: *std.build.Builder) void {
+    // At the moment we can't control the CPU features using the command line `zig build`
+    // and one of my targets for this program is an APU2 router which doesn't have the features below.
+    //
+    // We disable the features by default instead and allow the user to build with native features using `zig build -Dtarget=native-linux`
+    var disabled_features = blk: {
+        var res = std.Target.Cpu.Feature.Set.empty;
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+        const Feature = std.Target.x86.Feature;
+
+        res.addFeature(@enumToInt(Feature.bmi2));
+        res.addFeature(@enumToInt(Feature.avx2));
+
+        break :blk res;
+    };
+
+    var default_target = std.zig.CrossTarget{
+        .cpu_arch = .x86_64,
+        .cpu_model = .{ .explicit = &std.Target.x86.cpu.haswell },
+        // TODO(vincent): until this code is fixed with .native the features are not disabled
+        // https://github.com/ziglang/zig/blob/master/lib/std/zig/cross_target.zig#L326-L356
+        // .cpu_model = .native,
+        .cpu_features_sub = disabled_features,
+        .os_tag = .linux,
+    };
+
+    const target = b.standardTargetOptions(.{
+        .default_target = default_target,
+    });
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("sshtrap", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }
