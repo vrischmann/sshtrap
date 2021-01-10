d3cfe4e02ec5a35d285e25042fed0ad557d1c4ca
Vincent Rischmann <vincent@rischmann.fr>
2021-01-10 04:29:14 +0100

    working version

--- src/main.zig
+++ src/main.zig
@@ -8,18 +8,24 @@ const os = std.os;
 const time = std.time;
 
 const max_clients = 1;
-const delay = 10 * time.ns_per_s;
-const port = 22;
+const delay = 1000;
+const port = 54650;
 
 const Session = struct {
+    addr: net.Address,
     socket: os.socket_t,
     socket_writer: SocketWriter,
+
+    statistics: struct {
+        connect_time: i64,
+        bytes_sent: usize,
+    },
 
     send_next: i64,
 };
 
 fn createServer() !os.socket_t {
-    const sockfd = try os.socket(os.AF_UNSPEC, os.SOCK_STREAM, 0);
+    const sockfd = try os.socket(os.AF_INET6, os.SOCK_STREAM, 0);
 
     // Enable reuseaddr if possible
     os.setsockopt(
@@ -37,9 +43,9 @@ fn createServer() !os.socket_t {
         &mem.toBytes(@as(c_int, 0)),
     );
 
-    const addr = net.Address.initIp4([_]u8{ 0x7F, 0x00, 0x00, 0x01 }, port);
+    const addr = try net.Address.parseIp6("::1", port);
 
-    try os.bind(sockfd, &addr.any, @sizeOf(os.sockaddr_in));
+    try os.bind(sockfd, &addr.any, @sizeOf(os.sockaddr_in6));
     try os.listen(sockfd, std.math.maxInt(u31));
 
     return sockfd;
@@ -48,7 +54,6 @@ fn createServer() !os.socket_t {
 fn socketWrite(fd: os.socket_t, bytes: []const u8) os.WriteError!usize {
     return os.write(fd, bytes);
 }
-
 const SocketWriter = io.Writer(os.socket_t, os.WriteError, socketWrite);
 
 const logger = std.log.scoped(.main);
@@ -65,55 +70,125 @@ pub fn main() anyerror!void {
 
     var sessions = std.ArrayList(Session).init(allocator);
 
+    // Create a PRNG
+
+    var rng = std.rand.DefaultPrng.init(@intCast(u64, time.milliTimestamp()));
+
+    // Ignore broken pipes
+    var act = os.Sigaction{
+        .handler = .{
+            .sigaction = os.SIG_IGN,
+        },
+        .mask = os.empty_sigset,
+        .flags = 0,
+    };
+    os.sigaction(os.SIGPIPE, &act, null);
+
     // Create the server
     const server_sockfd = try createServer();
 
     // Accept connections indefinitely
     while (true) {
-        // Enqueue clients
-        var timeout: i32 = -1;
-        const now = time.timestamp();
-        for (sessions.items) |session| {
-            if (session.send_next <= now) {
-                // TODO(vincent): send random line
-                _ = try session.socket_writer.writeAll("foobar");
+        // Write random data for sessions that are due for another message.
+        const now = time.milliTimestamp();
 
-                logger.debug("wrote line for socket {}", .{session.socket});
-            } else {
-                timeout = @intCast(i32, session.send_next - now);
+        var i: usize = 0;
+        while (i < sessions.items.len) : (i += 1) {
+            var session = &sessions.items[i];
+            if (session.send_next > now) continue;
 
-                logger.debug("timeout: {}", .{timeout});
+            // Create random message
+            var message_buffer: [32]u8 = undefined;
+            rng.random.bytes(&message_buffer);
 
-                break;
-            }
+            session.socket_writer.writeAll(&message_buffer) catch |err| {
+                logger.info("CLOSE host={} port={} fd={} elapsed={d:.3} bytes={} error={}", .{
+                    session.addr,
+                    session.addr.getPort(),
+                    session.socket,
+                    @intToFloat(f32, now - session.statistics.connect_time) / 1e3,
+                    session.statistics.bytes_sent,
+                    err,
+                });
+
+                // Remove the current session
+                _ = sessions.orderedRemove(i);
+                if (i > 0) i -= 1;
+
+                continue;
+            };
+
+            session.send_next = now + delay;
+            session.statistics.bytes_sent += message_buffer.len;
         }
+
+        // Compute the earliest timeout
+        const timeout: i32 = blk: {
+            if (sessions.items.len <= 0) break :blk -1;
+
+            var earliest_timeout: i32 = std.math.maxInt(i32);
+            for (sessions.items) |session| {
+                earliest_timeout = std.math.min(
+                    earliest_timeout,
+                    @intCast(i32, session.send_next - now),
+                );
+            }
+            break :blk earliest_timeout;
+        };
 
         // Wait for next event
 
-        var fd = .{
+        var fds = [_]os.pollfd{.{
             .fd = server_sockfd,
             .events = os.POLLIN,
             .revents = 0,
-        };
+        }};
 
-        _ = try os.poll(&[_]os.pollfd{fd}, timeout);
+        const events = if (sessions.items.len < max_clients)
+            try os.poll(&fds, timeout)
+        else
+            try os.poll(&[_]os.pollfd{}, timeout);
+        if (events < 1) {
+            // No event, retry
+            continue;
+        }
+
+        var fd = &fds[0];
         if (fd.revents & os.POLLIN == os.POLLIN) {
+            var client_addr: net.Address = undefined;
+            var client_addr_len: os.socklen_t = @sizeOf(net.Address);
+
             const client_fd = try os.accept(
                 server_sockfd,
-                null,
-                null,
+                &client_addr.any,
+                &client_addr_len,
                 os.SOCK_NONBLOCK,
             );
 
-            logger.info("accepted client {}", .{client_fd});
+            logger.info("ACCEPT host={} port={} fd={} n={}/{}", .{
+                client_addr,
+                client_addr.getPort(),
+                client_fd,
+                sessions.items.len + 1,
+                max_clients,
+            });
 
-            var session = .{
+            var socket_writer = .{ .context = client_fd };
+
+            const now2 = time.milliTimestamp();
+
+            var session = Session{
+                .addr = client_addr,
                 .socket = client_fd,
-                .socket_writer = .{ .context = client_fd },
+                .socket_writer = socket_writer,
 
-                .send_next = time.timestamp() + delay,
+                .statistics = .{
+                    .connect_time = now2,
+                    .bytes_sent = 0,
+                },
+
+                .send_next = now2 + delay,
             };
-
             try sessions.append(session);
         }
     }
